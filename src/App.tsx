32dfb16edf3b06e71964039255ed94dcfe5d012e
arturkeil <kyleartur@gmail.com>
2024-03-01 02:38:29 +0500

    adding empty components and icons

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Tree } from './Components/Tree/Tree.tsx';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Tree />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Tree/Tree.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import styles from './Tree.module.css'
+
+export const Tree = () => {
+    return <div className={styles['wrapper']} >Tree Component</div>
+}
